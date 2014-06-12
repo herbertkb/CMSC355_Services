462b644bd7872f2bf94e819a69e66692e81ae54b
Keith Herbert <herbertkb@vcu.edu>
2014-06-12 17:36:27 -0400

    Dictionary Fix 0008
    Add more console output.
    Allow logging to external file.
    Change tabs to spaces for github readability.
    Minor change to commenting style

--- Dictionary/Dictionary.java
+++ Dictionary/Dictionary.java
@@ -1,163 +1,268 @@
 /******************************************************************************
-	Dictionary.java
-	
-	Simple program that takes an English word and a translation file as input
-	and returns the word's translation as output. 
-	
-	Translation file must be formated as:
-	*QueryWord* *tab* *TranslatedWord*
-	
+    Dictionary.java
+    
+    Simple program that takes an English word and a translation file as input
+    and returns the word's translation as output. 
+    
+    Translation file must be formated as:
+    *QueryWord* *tab* *TranslatedWord*
+    
 *******************************************************************************
-	To run:
-		java Dictionary *englishWord* *translationFile*
-		
-	Example:
-		> java Dictionary boy English2Russian.txt
-		мальчик
-		>
-		> java Dictionary fnord Englist2Russian.txt
-		FNORD not found in translation file "English2Russian.txt". 
+    To run:
+        java Dictionary *englishWord* *translationFile*
+        
+    Example:
+        > java Dictionary boy English2Russian.txt
+        мальчик
+        >
+        > java Dictionary fnord Englist2Russian.txt
+        FNORD not found in translation file "English2Russian.txt". 
 
 *******************************************************************************
-	Maintenance Log
-	Fix #	Date		Name	Description
-	0001	20 May 2014	Keith	First draft. Many back and forth revisions to 
-								fix issues with Cyrillic output to Windows
-								console. 
-	0002	21 May 2014	Keith	More fiddling to fix Cyrillic output to 
-								Windows console. 
-	0003	25 May 2014	Keith	Minor variable name changes for clarity.
-	0004	26 May 2014	Keith	Change from Unix style command line utility
-								to interactive server. Begin by reading whole 
-								word file into memory and sort it. Then 
-								binary search for word translation in response
-								to input.
-	0005	10 Jun 2014	Keith	Tear out and replace changes from Fix#0004 with
-								simpler and faster Map data structure.
-	0006	10 Jun 2014	Keith	Combined a few lines together in the loop which
-								reads the translation file. More comments for
-								main loop.
-	0007	11 Jun 2014	Keith	Transition from local daemon server to network
-								server. 
-								Exception handling for reading in the translation
-								file. 
+    Maintenance Log
+    Fix #   Date        Name    Description
+    0001    20 May 2014 Keith   First draft. Many back and forth revisions to 
+                                fix issues with Cyrillic output to Windows
+                                console. 
+    0002    21 May 2014 Keith   More fiddling to fix Cyrillic output to 
+                                Windows console. 
+    0003    25 May 2014 Keith   Minor variable name changes for clarity.
+    0004    26 May 2014 Keith   Change from Unix style command line utility
+                                to interactive server. Begin by reading whole 
+                                word file into memory and sort it. Then 
+                                binary search for word translation in response
+                                to input.
+    0005    10 Jun 2014 Keith   Tear out and replace changes from Fix#0004 with
+                                simpler and faster Map data structure.
+    0006    10 Jun 2014 Keith   Combined a few lines together in the loop which
+                                reads the translation file. More comments for
+                                main loop.
+    0007    11 Jun 2014 Keith   Transition from local daemon server to network
+                                server. 
+                                Exception handling for reading in the translation
+                                file.
+    0008    12 Jun 2014 Keith   Add more console output.
+                                Allow logging to external file.
+                                Change tabs to spaces for github readability.
+                                Minor change to commenting style
 
 ******************************************************************************/
 
-import java.util.Scanner;			// to parse the translation file
-import java.io.File;				// to work with scanner to open trans file
-import java.io.PrintStream; 		// to force printing in Unicode
-import java.util.HashMap;			// implementation of Map. Fastest lookup.
-import java.util.Map;				// Abstract data structure for mapping
+import java.util.Scanner;           // to parse the translation file
+import java.io.File;                // to work with scanner to open trans file
+import java.io.PrintStream;         // to force printing in Unicode
+import java.util.HashMap;           // implementation of Map. Fastest lookup.
+import java.util.Map;               // Abstract data structure for mapping
 import java.net.*;
 import java.util.*;
 import java.io.*;
 
 public class Dictionary {
-	public static void main(String[] args) 
-		throws java.io.FileNotFoundException, 
-			   java.io.UnsupportedEncodingException {
-		
-		/*	Parse commandline arguments.///////////////////////////////////////
-		//	args[0] must be the filename for the translation file.
-		//	args[1] must be the port number to listen for incoming connections.
-		*/
-		String transFile = args[0];
-		int port = Integer.parseInt(args[1]);
-		
-		///////////////////////////////////////////////////////////////////////
-		/** Open the translation file and read every line into memory as a 
-		// key,value pair with the query word as the key and the translation
-		// as the value, represented in the translation file by two words
-		// separated by a tab.
-		*/
-		Map<String, String> wordMap = new HashMap<String, String>();
-		try (Scanner parser = new Scanner(new File(transFile), "UTF-8")) {
-			System.out.println("Processing " + transFile + ".");
-			while (parser.hasNextLine()) {
-				String[] fields = parser.nextLine().trim().split("\t");
-				wordMap.put(fields[0], fields[1]);
-			}
-			System.out.println(transFile + " processed.");				
-		} catch (FileNotFoundException e) {
-			System.out.println("Error reading translation file " + transFile);
-			System.out.println(e.toString());
-		}
-		
-		try ( ServerSocket serverSocket = new ServerSocket( port )) {
-			
-			System.out.println("Dictionary server started at " + new Date());
-			int clientCount = 0;
-			
-			while (true) {
-				Socket clientSocket = serverSocket.accept();
-				clientCount++;
-				InetAddress clientIP = clientSocket.getInetAddress();
-				String clientHostName = clientIP.getHostName();
-				
-				System.out.println("Start connection for client " + clientCount
-					+ " at " + new Date());
-				System.out.println("Client " + clientCount + " hostname: " 
-					+ clientHostName);
-						
-				//HandleClient clientTask = new HandleClient(clientSocket, wordMap);
-				//new Thread(clientTask).start();
-				
-				Thread client = new Thread(new HandleClient(clientSocket, wordMap));
-				client.start();
-			}
-		} catch (IOException e) {
-			System.err.println(e);
-		}
-	}
-}	
-class HandleClient implements Runnable {
-	
-	private Socket socket;
-	private Map wordMap;
-	
-	public HandleClient(Socket socket, Map wordMap ) {
-		this.socket = socket;
-		this.wordMap = wordMap;
-	}
-	
-	public void run() {
-	
-		try (
-			BufferedReader input = new BufferedReader(
-				new InputStreamReader(socket.getInputStream()));
-				
-			PrintWriter output = new PrintWriter(
-				socket.getOutputStream(), true);	// true for autoFlush
-				
-			//Scanner input = new Scanner(socket.getInputStream());
-			//PrintStream output = new PrintStream(socket.getOutputStream);
-		) {
-		///////////////////////////////////////////////////////////////////////
-		/** Create input and output objects as Unicode to/from System In/Out.
-		// This is needed to force Unicode over local encodings and handle
-		// non-Latin characters.
-		*/
-		//Scanner input = new Scanner(System.in, "UTF-8");
-		//PrintStream output = new PrintStream(System.out, true, "UTF-8");
-		
-		/** Main loop //////////////////////////////////////////////////////////
-		// Take query word from System.in and check if it exists in the word
-		// map. If it does, print the translation out to the console. 
-		// Otherwise, print an error message. 
-		
-		*/
-			while ( true ) {
-				String query = input.readLine();
-				if (wordMap.containsKey( query )) {	
-					output.println( wordMap.get( query.toLowerCase() ) ); 
-				}
-				else { 
-					output.println("No translation for " + query.toUpperCase()); 
-				}
-			}
-		} catch (IOException e) {
-			System.err.println(e);
-		}
-	}
-}
+
+    private static boolean LOG = false; // flag for logging
+    private static PrintWriter logOut;  // output to logfile if LOG is true
+
+    public static void main(String[] args) 
+        throws java.io.FileNotFoundException, 
+               java.io.UnsupportedEncodingException {
+        
+        /**********************************************************************  
+            Parse commandline arguments:
+            args[0] must be the filename for the translation file.
+            args[1] must be the port number to listen for incoming connections.
+            args[2] may be the filename for a log file (OPTIONAL)
+        **********************************************************************/
+        String transFile = args[0];
+        int port = Integer.parseInt(args[1]);
+        
+        if (args.length == 3) {
+            String logfile = args[2];
+            LOG = true;
+            logOut = new PrintWriter(logfile, "UTF-8");
+        }
+        
+        
+        /**********************************************************************
+        /* Open the translation file and read every line into memory as a 
+        /* key,value pair with the query word as the key and the translation
+        /* as the value, represented in the translation file by two words
+        /* separated by a tab.
+        /*********************************************************************/
+        Map<String, String> wordMap = new HashMap<String, String>();
+        
+        try (Scanner parser = new Scanner(new File(transFile), "UTF-8")) {
+            
+            String processedStart = new Date() + " Processing " + transFile + ".";
+            System.out.println( processedStart );
+            if (LOG) { 
+                logOut.println( processedStart ); 
+                logOut.flush();
+            }
+            
+            while (parser.hasNextLine()) {
+                String[] fields = parser.nextLine().trim().split("\t");
+                wordMap.put(fields[0], fields[1]);
+            }
+           
+           String processedWin = new Date() + " " + transFile + " processed.";     
+           System.out.println( processedWin );
+           if (LOG) { 
+            logOut.println( processedWin ); 
+            logOut.flush();
+            }
+        } catch (FileNotFoundException e) {
+            String processedFail = new Date() + "Error reading translation file "
+                + transFile +"\n" + e.toString();
+            
+            System.out.println( processedFail );
+            if (LOG) { 
+                logOut.println( processedFail );
+                logOut.flush();
+            }
+            
+        }
+        
+        
+        /********************************************************************** 
+        /* Open a port to listen for incoming connections. 
+        /* On each connection, spawn a new thread to handle the client's needs.
+        /*********************************************************************/
+        try ( ServerSocket serverSocket = new ServerSocket( port )) {
+            
+            String serverStart = 
+                new Date() + " Dictionary server listening on port " + port + ".";
+            System.out.println( serverStart );
+            if (LOG) {
+                logOut.println( serverStart );
+                logOut.flush();
+            }
+            
+            int clientCount = 0;
+            
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                clientCount++;
+                InetAddress clientIP = clientSocket.getInetAddress();
+                String clientHostName = clientIP.getHostName();
+                
+                String clientConnect = 
+                    new Date() + " Client " + clientCount + " (" + clientHostName 
+                    + ") connected.";
+                System.out.println( clientConnect );
+                logOut.println( clientConnect );
+                
+                Thread client = new Thread(
+                    new HandleClient(clientSocket, wordMap, clientCount));
+                client.start();
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+        
+        
+        if (LOG) { logOut.close(); }
+        
+    } // end main method
+
+/******************************************************************************
+/* HandleClient - inner class required to create runnable threads.
+/* Having each client on its own thread allows the Dictionary server to handle
+/* many clients at once. 
+/*****************************************************************************/    
+private static class HandleClient implements Runnable {
+    
+    private Socket socket;      // the socket for the client connection.
+    private Map<String, String> wordMap;        // collection of words and translations
+    private int clientID;       // ID# for client. Useful for logging.
+    
+    // Constructor
+    public HandleClient(Socket socket, Map<String, String> wordMap, int clientID ) {
+        this.socket = socket;
+        this.wordMap = wordMap;
+        this.clientID = clientID;
+    }
+    
+    
+    /************************************************************************** 
+    /* The run() method is called by JVM to start a new thread for each client
+    /* connection. It's where all the magic happens after a client connects. 
+    /*************************************************************************/
+    public void run() {
+        try (
+            // Create objects to handle client input/output 
+            BufferedReader clientIn = new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));
+                
+            PrintWriter clientOut = new PrintWriter(
+                socket.getOutputStream(), true);    // 'true' is for autoFlush
+        ) {
+        
+        /** *******************************************************************
+        /* Main loop
+        /* Take query word from System.in and check if it exists in the word
+        /* map. If it does, print the translation out to the client. 
+        /* Otherwise, print an error message. 
+        /*********************************************************************/
+            while ( true ) {
+                // Grab the word from the user.
+                String query = clientIn.readLine();
+                
+                String clientQueryMsg = 
+                    new Date() + " Client " + clientID + " query: " + query;
+                System.out.println( clientQueryMsg );
+                if (LOG) {
+                    logOut.println( clientQueryMsg );
+                    logOut.flush();
+                }
+                
+                // Look the word up and return translation to client.
+                if (wordMap.containsKey( query )) {
+                    
+                    String translation =  wordMap.get( query.toString() );
+                    clientOut.println( translation ); 
+                    
+                    String clientTransMsg = 
+                        new Date() + " Client " + clientID + " translation: " +
+                        translation;
+                    System.out.println( clientTransMsg );
+                    if (LOG) {
+                        logOut.println( clientTransMsg );
+                        logOut.flush();
+                    }
+                }
+                // null if client has disconnected. End the loop.
+                else if (query == null ) { break; }
+                
+                // Error message if word not found in collection. 
+                else { 
+                    clientOut.println("No translation for " + query.toUpperCase());
+                    
+                    String queryFail = new Date() + " Client " + clientID 
+                        + ": No translation for " + query.toUpperCase();
+                    System.out.println(queryFail);
+                    if (LOG) {
+                        logOut.println(queryFail);
+                        logOut.flush();
+                    }
+                }
+            }
+            
+        } catch (IOException e) { 
+            System.out.println(e); 
+            if (LOG) { 
+                logOut.println(e);
+                logOut.flush();
+            }
+        }
+        
+        // Disconnect message.
+        String disconnectMsg = new Date() + " Client " + clientID + " disconnected.";
+        System.out.println( disconnectMsg );
+        if (LOG) {
+            logOut.println(disconnectMsg);
+            logOut.flush();
+        }
+    }
+} // end HandleClient class
+
+} // end Dictionary class
